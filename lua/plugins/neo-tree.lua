e95ee7537fdd1ffc70209e4923e9296310337dfa
Pedro Orlando <porlando@Pedros-MacBook-Pro.local>
2024-08-05 19:09:15 -0300

    back to stability

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,6 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-    "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
 }
