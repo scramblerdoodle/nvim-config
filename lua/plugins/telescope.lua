52f7f18dbfb98cc13692c7a6129644bec905648e
Pedro Orlando <scramblerdoodle@gmail.com>
2024-08-06 15:56:52 -0300

    no more trash, telescope browse plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,13 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    -- defaults = {
-    --   layout_strategy = "horizontal",
-    --   layout_config = { prompt_position = "top" },
-    --   sorting_strategy = "ascending",
-    --   winblend = 0,
-    -- },
+  keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
