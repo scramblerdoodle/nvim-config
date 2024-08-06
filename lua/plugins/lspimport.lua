4dc730e0b981f574f0f948bd112e59ed356586d3
Pedro Orlando <porlando@riseup.net>
2024-08-06 17:25:48 -0300

    autoimports

---
+++ lua/plugins/lspimport.lua
@@ -0,0 +1,9 @@
+return {
+  -- Autoimport I hope
+  {
+    "stevanmilic/nvim-lspimport",
+    config = function()
+      vim.keymap.set("n", "<leader>ci", require("lspimport").import, { desc = "Fix Import", noremap = true })
+    end,
+  },
+}
