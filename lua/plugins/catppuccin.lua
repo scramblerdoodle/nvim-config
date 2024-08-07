278fafcaf760a6ca7470c999f510f1d866e21841
Pedro Orlando <scramblerdoodle@gmail.com>
2024-08-07 17:02:21 -0300

    catppuccin and json formatter

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,7 +3,4 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  opts = {
-    colorscheme = "catppuccin-mocha",
-  },
 }
---
+++ lua/plugins/nvim-lsp.lua
@@ -0,0 +1,29 @@
+return {
+  -- Mason config
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      -- vim.list_extend(opts.ensure_installed, { "pyright", "black", "ruff-lsp", "ruff" })
+      vim.list_extend(opts.ensure_installed, {
+        "black",
+        "ruff",
+        "debugpy",
+        -- "mypy",
+        "ruff-lsp",
+        "pyright",
+      })
+    end,
+  },
+
+  -- Setup up format with new `conform.nvim`
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["python"] = { "ruff", "black", "isort" },
+        ["json"] = { "jq" },
+      },
+    },
+  },
+}
