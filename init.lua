8e169e25d1e563b6906ee339becc32a9c67bc957
Pedro Orlando <porlando@riseup.net>
2024-08-15 20:34:02 -0300

    Adding dictionary support + small which-key fix

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Spell --
+-- TODO: Add this to a proper file, don't just leave it in init.lua
+--       Also, choose a pretty nerd-font icon for this group
+local wk = require("which-key")
+wk.add({ "<leader>z", group = "language" })
