988ec62fb00278c639322806fa86927264bd0587
Pedro Orlando <porlando@riseup.net>
2026-04-09 02:15:50 -0300

    WISHLIST
    Adding comment to `init.lua` because of reasons
    It contains some future plans
    
    Signed-off-by: Pedro Orlando <scramblerdoodle@gmail.com>

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- WISHLIST:
+-- 1. Fix cyberdream colourscheme on Snacks.dashboard (buttons are not using the correct colours)
+-- 2. Fix Kotlin neotest setup (not running all tests (<l>tT))
+-- 3. Probably need to figure out the Kotlin DAP too
+-- 4. Improve DAP/neotest usability as a whole, I think there are a lot of features I'm not using
+-- 5. Learn to use marks better
+-- 6. Make a custom plugin that extends from Snacks.nvim and allows the user to choose their dashboard art from a custom
+-- 		list/table defined in a file
+-- 7. Better lazygit integration
+-- 		- currently starting up a bunch of different tabs/workspaces
+-- 		- terrible experience at handling conflicts
+-- 		- try to add custom script for getting the message templates
+-- 8. Init nvim with the dashboard even if we pass it a directory
